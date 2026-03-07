2555be0efd194074d9ef5744c8b20a071292a50c
Prcardoso1 <paulo.rogeriomcardoso@gmail.com>
2026-03-07 20:04:20 -0300

    Translate SetPasswordModal to Portuguese

--- app/src/components/modals/SetPasswordModal.vue
+++ app/src/components/modals/SetPasswordModal.vue
@@ -1,93 +1,94 @@
 <template>
-  <Modal :isOpen="isOpen" @close="$emit('cancel')" title="Set New Password">
+  <Modal :isOpen="isOpen" @close="$emit('cancel')" title="Definir nova senha">
     <template #default>
       <p class="modal-description">
-        Your password has been reset by an administrator. Please set a new password to continue.
+        Sua senha foi redefinida por um administrador. Defina uma nova senha para continuar.
       </p>
 
       <form @submit.prevent="handleSubmit">
         <FormInput
           v-model="usernameDisplay"
-          label="Username"
+          label="Usuário"
           type="text"
           readonly
         />
+
         <FormInput
           v-model="newPasswordInput"
-          label="New Password"
+          label="Nova senha"
           type="password"
-          placeholder="Enter new password (min 6 characters)"
+          placeholder="Digite a nova senha (mínimo 6 caracteres)"
           :error="error"
         />
+
         <FormInput
           v-model="confirmPasswordInput"
-          label="Confirm Password"
+          label="Confirmar senha"
           type="password"
-          placeholder="Confirm new password"
+          placeholder="Confirme a nova senha"
         />
       </form>
     </template>
+
     <template #footer>
-      <button class="btn-success" @click="handleSubmit">Set Password</button>
-      <button class="btn-secondary" @click="$emit('cancel')">Cancel</button>
+      <button class="btn-success" @click="handleSubmit">Definir senha</button>
+      <button class="btn-secondary" @click="$emit('cancel')">Cancelar</button>
     </template>
   </Modal>
 </template>
 
 <script setup>
-import { ref, watch } from 'vue';
-import Modal from '@/components/common/Modal.vue';
-import FormInput from '@/components/common/FormInput.vue';
+import { ref, watch } from 'vue'
+import Modal from '@/components/common/Modal.vue'
+import FormInput from '@/components/common/FormInput.vue'
 
 /**
- * SetPasswordModal - Modal for setting a new password after admin reset
+ * SetPasswordModal - Modal para definir nova senha após reset do administrador
  *
- * @emits submit - When user submits new password (emits {newPassword, confirmPassword})
- * @emits cancel - When user cancels password setup
+ * @emits submit - Quando usuário envia nova senha
+ * @emits cancel - Quando usuário cancela
  */
+
 const props = defineProps({
-  /** Whether the modal is open */
   isOpen: {
     type: Boolean,
     required: true
   },
-  /** Username to display (readonly) */
+
   username: {
     type: String,
     required: true
   },
-  /** Error message to display */
+
   error: {
     type: String,
     default: ''
   }
-});
+})
 
-const emit = defineEmits(['submit', 'cancel']);
+const emit = defineEmits(['submit', 'cancel'])
 
-const usernameDisplay = ref(props.username);
-const newPasswordInput = ref('');
-const confirmPasswordInput = ref('');
+const usernameDisplay = ref(props.username)
+const newPasswordInput = ref('')
+const confirmPasswordInput = ref('')
 
-// Watch for username prop changes
 watch(() => props.username, (newVal) => {
-  usernameDisplay.value = newVal;
-});
+  usernameDisplay.value = newVal
+})
 
-// Clear passwords when modal closes
 watch(() => props.isOpen, (isOpen) => {
   if (!isOpen) {
-    newPasswordInput.value = '';
-    confirmPasswordInput.value = '';
+    newPasswordInput.value = ''
+    confirmPasswordInput.value = ''
   }
-});
+})
 
 function handleSubmit() {
   if (newPasswordInput.value.trim() && confirmPasswordInput.value.trim()) {
     emit('submit', {
       newPassword: newPasswordInput.value,
       confirmPassword: confirmPasswordInput.value
-    });
+    })
   }
 }
 </script>
